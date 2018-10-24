6b4070ce668e7761e16264dc0ba2954f38497d2a
papnoisanjeev <sanjeevpapnoi829@webkul.com>
2018-10-24 18:38:02 +0530

    update thread

--- Controller/Thread.php
+++ Controller/Thread.php
@@ -1,23 +1,18 @@
 <?php
-
 namespace Webkul\UVDesk\CoreBundle\Controller;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 class Thread extends Controller
 {
     public function saveThread($ticketId)
     {
         $entityManager = $this->getDoctrine()->getManager();
         $request = $this->container->get('request_stack')->getCurrentRequest();
-
         $params = $request->request->all();
         $ticket = $entityManager->getRepository('UVDeskCoreBundle:Ticket')->findOneById($ticketId);
-
         // Validate Request
         if (empty($ticket)) {
             throw new \Exception('Ticket not found', 404);
@@ -30,19 +25,15 @@ class Thread extends Controller
                     throw new \Exception('Insufficient Permisions', 400);
                 }
             }
-
             // // Deny access unles granted ticket view permission
             // $this->denyAccessUnlessGranted('AGENT_VIEW', $ticket);
-
             // Check if reply content is empty
             $parsedMessage = trim(strip_tags($params['reply'], '<img>'));
             $parsedMessage = str_replace('&nbsp;', '', $parsedMessage);
             $parsedMessage = str_replace(' ', '', $parsedMessage);
-
             if (null == $parsedMessage) {
                 $this->addFlash('warning', "Reply content cannot be left blank.");
             }
-
             // @TODO: Validate file attachments
             // if (true !== $this->get('file.service')->validateAttachments($request->files->get('attachments'))) {
             //     $this->addFlash('warning', "Invalid attachments.");
@@ -55,6 +46,7 @@ class Thread extends Controller
             'source' => 'website',
             'threadType' => strtolower($params['threadType']),
             'message' => $params['reply'],
+            'attachments' => $request->files->get('attachments')
         ];
         
         if(isset($params['to']))
@@ -63,46 +55,35 @@ class Thread extends Controller
         // Create Thread
         $thread = $this->get('ticket.service')->createThread($ticket, $threadDetails);
         // $this->addFlash('success', ucwords($params['threadType']) . " added successfully.");
-
         // @TODO: Remove Agent Draft Thread
         // @TODO: Trigger Thread Created Event
         // dump($thread);
         
         $ticket->createdThread = $thread;
-
         $event = new GenericEvent('ticket.replyAgent', [
             'entity' => $ticket,
             'event' => 'ticket.replyAgent',
         ]);
-
         $this->get('event_dispatcher')->dispatch('uvdesk.automation.workflow.execute', $event);
         // $this->get('uvdesk.automations')->trigger([
         //     'entity' => $ticket,
         //     'event' => 'ticket.reply.added'
         // ]);
-        
-        die;
         // Check if ticket status needs to be updated
         $updateTicketToStatus = !empty($params['status']) ? (trim($params['status']) ?: null) : null;
-
         if (!empty($updateTicketToStatus) && $this->get('user.service')->isAccessAuthorized('ROLE_AGENT_UPDATE_TICKET_STATUS')) {
             $ticketStatus = $em->getRepository('UVDeskCoreBundle:TicketStatus')->findOneById($updateTicketToStatus);
-
             if (!empty($ticketStatus) && $ticketStatus->getId() === $ticket->getStatus()->getId()) {
                 $ticket->setStatus($ticketStatus);
-
                 $entityManager->persist($ticket);
                 $entityManager->flush();
-
                 // @TODO: Trigger Ticket Status Updated Event
             }
         }
-
         // Redirect to either Ticket View | Ticket Listings
         if ('redirect' === $params['nextView']) {
-            return $this->redirect($this->generateUrl('thelpdesk_member_ticket_collection'));
+            return $this->redirect($this->generateUrl('helpdesk_member_ticket_collection'));
         }
-
         return $this->redirect($this->generateUrl('helpdesk_member_ticket', ['ticketId' => $ticket->getId()]));
     }
 }
